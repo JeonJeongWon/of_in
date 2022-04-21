9a14742c1533e9284a14dbe7682e509230e40b9e
sang0436 <63089782+sang0436@users.noreply.github.com>
2022-04-21 10:38:41 +0900

    All CLEAR

--- main.py
+++ main.py
@@ -22,5 +22,24 @@ from salehAmp import *
 # #
 # plt.plot(in_pw,out_pw)
 # plt.show()
+# ------------------
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+# ------------------
+
 train_data, test_data = data_generate()
 model_loss_1 = train(0,train_data,test_data)  # 0 : loss_1 only training, # 1 : loss_1 + papr training
+
+model_loss_1.encoder.save("Encoder_loss1.h5")
+model_loss_1.decoder.save("Decoder_loss1.h5")
+
--- variables.py
+++ variables.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--num_sc",default=64,type=int,help="Number Of Subarriers")
 parser.add_argument("--num_ch_paths",default=2,type=int,help="Number Of Channel paths")
 parser.add_argument("--bps",default=2,type=int,help="Number Of Bits per Symbol")
-parser.add_argument("--num_datas",default=2000,type=int,help="Number Of Data")
+parser.add_argument("--num_datas",default=20,type=int,help="Number Of Data")
 #parser.add_argument("--FFTsize",default=)
 
 # channel parameter
@@ -19,7 +19,7 @@ parser.add_argument("--snrdb",default=10,type=int,help="SNRdB")
 ## NN parameter
 parser.add_argument("--num_nodes",default=2048,type=int,help='Number of Nodes in Layer')
 parser.add_argument("--batch_size",default=64, type=int,help="Batch Size")
-parser.add_argument("--epochs",default=1000,type=int,help="Epochs")
+parser.add_argument("--epochs",default=100,type=int,help="Epochs")
 parser.add_argument("--weight",default=0.01,type=float,help="Weights for loss fumction")
 parser.add_argument("--lr",default=1e-4,type=float,help="Learning Rate")
 
